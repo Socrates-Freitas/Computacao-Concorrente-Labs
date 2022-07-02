eb07d74cafbbe54ff328ee931b258e7cf980593f
Sócrates Freitas <68630680+Socrates-Freitas@users.noreply.github.com>
2022-07-01 21:00:27 -0300

    Create lab9.c

---
+++ Lab 9/lab9.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define NUMTHREADS 5
+
+// Globais
+sem_t semaforoT1,semaforoT24, semaforoT5;
+int estado_dialogo = 0;
+
+void *tarefaThread234(void *arg){ // intermediários
+    char *textoArgumento = (char *) arg; 
+
+    sem_wait(&semaforoT24);
+
+    puts(textoArgumento);
+    estado_dialogo++;
+     if(estado_dialogo == 4){
+       sem_post(&semaforoT1);
+    }
+
+    sem_post(&semaforoT24);
+    
+    pthread_exit(NULL);//
+}
+
+void *tarefaThread1(void *arg){ // aqui executa por último
+    char *textoArgumento = (char *) arg;
+
+    sem_wait(&semaforoT1);    
+   
+    puts(textoArgumento);
+
+    pthread_exit(NULL);
+}
+
+void *tarefaThread5(void *arg){ // aqui executa primeiro
+    char *textoArgumento = (char *) arg;
+
+    sem_wait(&semaforoT5);
+    puts(textoArgumento);
+
+    estado_dialogo++;
+    sem_post(&semaforoT24);
+
+    pthread_exit(NULL);
+}
+
+int main(){
+    char *frases[5] = {"Volte sempre!","Fique a vontade.","Sente-se por favor.",
+    "Aceita um copo d' água?","Seja bem vindo!"};
+
+    pthread_t identificadores[NUMTHREADS];
+
+    sem_init(&semaforoT24,0,0);
+    sem_init(&semaforoT1,0,0);
+    sem_init(&semaforoT5,0,1);
+
+
+    pthread_create(&identificadores[0],NULL,tarefaThread1,(void *) frases[0]); // iniciando thread 1
+    
+    for(int i = 1; i < NUMTHREADS - 1; i++){ // iniciando threads 2-4
+        pthread_create(&identificadores[i],NULL, tarefaThread234, (void *) frases[i]);
+    }
+   
+    pthread_create(&identificadores[4],NULL,tarefaThread5,(void *) frases[NUMTHREADS - 1] ); // iniciando thread 5
+
+    for(int i = 0; i < NUMTHREADS; i++){
+        pthread_join(identificadores[i],NULL);
+    }
+    
+    sem_destroy(&semaforoT1);
+    sem_destroy(&semaforoT24);
+    sem_destroy(&semaforoT5);
+
+
+
+    pthread_exit(NULL);
+    return 0;
+}
